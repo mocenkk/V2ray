84d5f8c7e195e7807cdca0a8be1e0872a07ce871
mocenkk <82760238+mocenkk@users.noreply.github.com>
2023-12-20 21:45:03 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ let userID = '667ae3d0-894d-476d-898b-9db1b47b367c';
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 
-let proxyIP = '103.139.175.207';
+let proxyIP = '38.47.186.29';
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
